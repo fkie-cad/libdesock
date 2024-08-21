c9cf1516d916a24a1e0410e26a727fbfe816162c
pd-fkie <77979557+pd-fkie@users.noreply.github.com>
2024-08-21 19:07:58 +0200

    Added listen()

---
+++ src/listen.c
@@ -0,0 +1,15 @@
+
+#include "util.h"
+#include "desock.h"
+#include "syscall.h"
+
+VISIBLE
+int listen (int fd, int backlog) {
+    if (UNLIKELY(!DESOCK_FD(fd))) {
+        return socketcall (listen, fd, backlog, 0, 0, 0, 0);
+    }
+    
+    DEBUG_LOG("listen(%d, %d)", fd, backlog);
+    fd_table[fd].listening = 1;
+    return 0;
+}
