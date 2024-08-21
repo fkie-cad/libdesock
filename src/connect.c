1ea2d1ef5a37ce5e4048941e50422be8ca7348d6
pd-fkie <77979557+pd-fkie@users.noreply.github.com>
2024-08-21 18:53:00 +0200

    Added connect()

---
+++ src/connect.c
@@ -0,0 +1,20 @@
+#include <sys/socket.h>
+
+#include "util.h"
+#include "desock.h"
+#include "syscall.h"
+
+#ifdef DESOCK_CONNECT
+
+VISIBLE
+int connect (int fd, const struct sockaddr* addr, socklen_t len) {
+    if (UNLIKELY(!has_supported_domain(fd))) {
+        return socketcall_cp (connect, fd, addr, len, 0, 0, 0);
+    }
+    
+    DEBUG_LOG("connect(%d, %p, %d)", fd, addr, len);
+    fd_table[fd].desock = 1;
+    return 0;
+}
+
+#endif
