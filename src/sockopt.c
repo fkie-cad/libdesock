fc640adac05f410cdd18b170b23819504190b3ca
pd-fkie <77979557+pd-fkie@users.noreply.github.com>
2024-08-28 15:06:34 +0200

    Added setsockopt()

---
+++ src/sockopt.c
@@ -0,0 +1,15 @@
+#include "util.h"
+#include "desock.h"
+#include "syscall.h"
+
+VISIBLE
+int setsockopt (int fd, int level, int optname, const void* optval, socklen_t optlen) {
+    if (UNLIKELY(!DESOCK_FD(fd))) {
+        int r = __socketcall (setsockopt, fd, level, optname, optval, optlen, 0);
+        return __syscall_ret (r);
+    }
+    
+    DEBUG_LOG("setsockopt(%d, %d, %d, %p, %lu)", fd, level, optname, optval, optlen);
+    return 0;
+}
+VERSION(setsockopt)
