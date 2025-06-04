0a3308a6225eeb0fc9146aa9d87a599e97b57125
pd-fkie <77979557+pd-fkie@users.noreply.github.com>
2025-06-04 23:18:56 +0200

    Fixed duplicate declaration of __syscall_cp_c()

--- src/syscall.c
+++ src/syscall.c
@@ -1,7 +1,5 @@
 #include "syscall.h"
 
-hidden long __syscall_cp_c ();
-
 static long sccp (syscall_arg_t nr, syscall_arg_t u, syscall_arg_t v, syscall_arg_t w, syscall_arg_t x, syscall_arg_t y, syscall_arg_t z) {
     return __syscall (nr, u, v, w, x, y, z);
 }
